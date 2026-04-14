9d09166b791b9ca600e28401a5e55265ce706185
pullasuti87 <84986118+tatti87@users.noreply.github.com>
2026-04-14 19:44:59 +0000

    green dot added

--- 3d_mapper.py
+++ 3d_mapper.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 import open3d as o3d
 
+video = "sample.mp4"
+cap = cv2.VideoCapture(video)
+
 # 3d visualizer
 vis = o3d.visualization.Visualizer()
 vis.create_window(window_name="SLAM", width=800, height=600)
@@ -10,20 +13,46 @@ vis.create_window(window_name="SLAM", width=800, height=600)
 opt = vis.get_render_option()
 opt.background_color = np.asarray([0, 0, 0])
 # point size
-opt.point_size = 1.0
+opt.point_size = 10.0
 
 # cloud object
 point_cloud = o3d.geometry.PointCloud()
 
-points = [[0.0, 0.0, 0.0]]
-# green starting point
-colors = [[0.0, 1.0, 0.0]] 
+# dummy points
+points = np.array([
+    [0.0, 0.0, 0.0],
+    [-1.0, -1.0, -1.0],
+    [1.0, 1.0, 1.0]
+], dtype=np.float64)
+
+colors = np.array([
+    [0.0, 1.0, 0.0],
+    [0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0]
+], dtype=np.float64)
+# ---------------------------------------------------------
+
 # needed so open3d will not crash, dumpy data
-point_cloud.points = o3d.utility.Vector3dVector(np.array(points))
-point_cloud.colors = o3d.utility.Vector3dVector(np.array(colors))
+point_cloud.points = o3d.utility.Vector3dVector(points)
+point_cloud.colors = o3d.utility.Vector3dVector(colors)
 vis.add_geometry(point_cloud)
 
+vis.reset_view_point(True)
 
-print(np.array(point_cloud.points))
-print(np.array(point_cloud.colors))
-print(vis)
+while True:
+    ret, frame = cap.read()
+
+    cv2.imshow("video", frame)
+
+    # prevets 3d window from freezing
+    vis.update_geometry(point_cloud)
+    vis.poll_events()
+    vis.update_renderer()
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# cleanup
+cap.release()
+cv2.destroyAllWindows()
+vis.destroy_window()
